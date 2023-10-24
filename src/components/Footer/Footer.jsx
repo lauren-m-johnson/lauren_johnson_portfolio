d775ee6fc57398b100dc9ab197a6813a44d776fb
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-24 15:20:30 -0700

    add footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,15 @@ import './Footer.css';
 export default function Footer() {
     return (
         <div id='footer'>
-            <h1>Footer</h1>
+            <h3>Contact</h3>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/lauren-marie-johnson/" target="_blank" rel='noreferrer'><img src="images/linkedin.png" /></a>
+                    <a href="https://github.com/lauren-m-johnson" target="_blank" rel='noreferrer'><img src="images/github.png" /></a>
+                </li>
+                <li>Icons created by<a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons"> Freepik - Flaticon</a></li>
+                <li>Website created and designed by Lauren Johnson</li>
+            </ul>
         </div>
     )
 }
