92875be8aefa7f3043551f4a9a6b16c0feddd67a
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-25 11:16:31 -0700

    add keen slider

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,13 @@
 import './Projects.css';
+import ProjectSlider from '../../components/ProjectSlider/ProjectSlider';
 
 export default function Projects() {
     return (
-        <div id='projects'>
-            <h1>Projects</h1>
+        <div id='main-container'>
+            <div id='projects'>
+                <h1>Projects</h1>
+                <ProjectSlider />
+            </div>
         </div>
     )
 }
