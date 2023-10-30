2a1e3bd7346ac1f01ed6fefa390e8594ce476076
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-30 15:44:21 -0700

    about page mobile responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,32 +4,32 @@ import './NavBar.css';
 
 export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
-  
+
     const toggleMenu = () => {
-      setIsOpen(!isOpen);
+        setIsOpen(!isOpen);
     }
-  
+
     return (
-      <div id='nav-bar'>
+    <div id='nav-bar'>
         <img src='/images/logo.png' alt='Logo with name, Lauren Johnson' />
         <div className='menu-icon' onClick={toggleMenu}>
-          <img src='/images/menu-bar.png' alt='Menu' />
+            <img src='/images/menu-bar.png' alt='Menu' />
         </div>
         {/* Render the individual links always */}
         <div id='nav-links'>
-          <Link to='/'>About</Link>
-          <Link to='/projects'>Projects</Link>
-          <Link to='/resume'>Resume</Link>
-          <Link to='/contact'>Contact</Link>
+            <Link to='/'>About</Link>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/resume'>Resume</Link>
+            <Link to='/contact'>Contact</Link>
         </div>
         {/* Render the collapsible menu only when isOpen is true */}
         {isOpen && (
-          <div id='nav-links' className={isOpen ? 'open' : ''}>
-            <Link to='/' onClick={toggleMenu}>About</Link>
-            <Link to='/projects' onClick={toggleMenu}>Projects</Link>
-            <Link to='/resume' onClick={toggleMenu}>Resume</Link>
-            <Link to='/contact' onClick={toggleMenu}>Contact</Link>
-          </div>
+            <div id='nav-links' className={isOpen ? 'open' : ''}>
+                <Link to='/' onClick={toggleMenu}>About</Link>
+                <Link to='/projects' onClick={toggleMenu}>Projects</Link>
+                <Link to='/resume' onClick={toggleMenu}>Resume</Link>
+                <Link to='/contact' onClick={toggleMenu}>Contact</Link>
+            </div>
         )}
       </div>
     )
